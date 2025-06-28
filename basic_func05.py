74246ef31aeac2d2f6d71c025d3eb2f67a48b5fb
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:42:37 -0400

    5 done

--- basic_func05.py
+++ basic_func05.py
@@ -1,4 +1,7 @@
 # Task: Create a function called return_float that returns a float value
+def return_float():
+    return (float())
+print(return_float())
 # Function should:
 # 1. Take no parameters
 # 2. Return any floating-point number
