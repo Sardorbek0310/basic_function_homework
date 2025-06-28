ea306fe374f6d9378fcd1769eebf83cd58e72207
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:58:37 -0400

    9 done

--- basic_func09.py
+++ basic_func09.py
@@ -1,4 +1,7 @@
 # Task: Create a function called decrease_by_one that decreases its input by 1
+def decrease_by_one(number):
+    return number-1
+print(decrease_by_one(0))
 # Function should:
 # 1. Take one parameter 'a'
 # 2. Subtract 1 from the value of 'a'
