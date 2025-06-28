ca7180b1b7ed5986561fa5f1c8fd5f21fd7f85f5
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:32:20 -0400

    3 done

--- basic_func03.py
+++ basic_func03.py
@@ -1,4 +1,7 @@
 # Task: Create a function called get_school_name that returns the string "codeschooluz"
+def get_school_name():
+    return("codeschooluz")
+print(get_school_name())
 # Function should:
 # 1. Take no parameters
 # 2. Return exactly "codeschooluz" as a string
