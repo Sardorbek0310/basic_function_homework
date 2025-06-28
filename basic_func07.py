88e90fa506b4cc93d5ca083c2e6beb6cdc583f7f
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:53:31 -0400

    7 done

--- basic_func07.py
+++ basic_func07.py
@@ -1,4 +1,7 @@
 # Task: Create a function called return_same_value that returns its input parameter
+def return_same_value(value):
+    return value
+print(return_same_value(' '))
 # Function should:
 # 1. Take one parameter 'a'
 # 2. Return the exact same value of 'a' without any modification
