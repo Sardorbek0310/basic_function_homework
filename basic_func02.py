cff1727e521d350e66f51def2e758795d04af19a
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:29:27 -0400

    2 done

--- basic_func02.py
+++ basic_func02.py
@@ -1,4 +1,7 @@
 # Task: Create a function called get_hello_world that returns the string "Hello World"
+def get_hello_world():
+    return("Hello world")
+print(get_hello_world())
 # Function should:
 # 1. Take no parameters
 # 2. Return exactly "Hello World" as a string
