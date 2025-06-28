8476f8f185eac6fb0f029b48be16e874626c0eb1
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 05:56:37 -0400

    8 done

--- basic_func08.py
+++ basic_func08.py
@@ -1,4 +1,7 @@
 # Task: Create a function called increase_by_one that increases its input by 1
+def increase_by_one(number):
+    return number+1
+print (increase_by_one(0))
 # Function should:
 # 1. Take one parameter 'a'
 # 2. Add 1 to the value of 'a'
