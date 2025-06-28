3f340a6d052ec168dd538f588e4fd57b15742987
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 06:03:24 -0400

    10 done

--- basic_func10.py
+++ basic_func10.py
@@ -1,4 +1,7 @@
 # Task: Create a function called get_opposite_value that returns the opposite of its input
+def get_opposite_value(number):
+    return number*-1
+print(get_opposite_value(5))
 # Function should:
 # 1. Take one parameter 'a'
 # 2. Return the negative value of 'a' (multiply by -1)
