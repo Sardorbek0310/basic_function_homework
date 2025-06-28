fce3fb7f6e6c0735c27e80a38f96d014f41c4491
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-06-28 10:19:31 -0400

    4 done

--- basic_func04.py
+++ basic_func04.py
@@ -1,7 +1,7 @@
 # Task: Create a function called return_integer that returns an integer value
-def return_integer():
-    return(int())
-print(return_integer())
+def return_integer(number):
+    return int(number)
+print(return_integer(1.9))
 # Function should:
 # 1. Take no parameters
 # 2. Return any integer value (positive or negative)
